fd1b2b38ee7a77d284aedbccb28b9b257b80f70c
Martin Abreu <martin.abreu@live.com>
2020-04-01 13:25:51 -0700

    Updates

---
+++ precache-manifest.4759f5f23a89f5b8058a3f9758d05d07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9ab9b19fdf552ae27bc25e461a29ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a75986bfda5acfc32f2",
+    "url": "/static/css/main.27bc7b66.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a75986bfda5acfc32f2",
+    "url": "/static/js/main.6894b105.chunk.js"
+  },
+  {
+    "revision": "a53c56bcba93a6115910",
+    "url": "/static/js/runtime-main.4595a359.js"
+  }
+]);
